6166bbeccade230647ca0c4180bd2b5720018db5
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-07-06 00:29:56 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # bearer-auth
+
+## Test CI:
+[URL]()
+
+## Heroku Link:
+[URL]()
+
+## Pull Request:
+[URL]()
+
+## Setup:
+1.clone the repo.
+2.npm init -y.
+3.npm exprees,dotenv,mongooes,cors,supuergooes,route,basic-64,bcrypt ,jsonwebtoken.
+
+
+### Endpoint:
+ - Auth routes for handling the login and authentication system:
+   - POST /signup to create an account
+   - POST /signin to login with Basic Auth
+
+
+
+## Test:
+.Unit test: npm test
+
+## Run:
+ - nodemon
+
+
+
+ ![auth](bearer-auth.jpg)
---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const base64 = require('base-64');
+const User = require('../models/users.js');
+
+module.exports = async (req, res, next) => {
+
+  if (!req.headers.authorization) { return 'error in authorization' }
+
+
+  let basic = req.headers.authorization.split(' ');
+  if (basic[0] !== 'Basic') { return 'Invalid Login' }
+  let [user, pass] = base64.decode(basic).split(':');
+
+  try {
+    req.user = await User.authenticateBasic(user, pass)
+    next();
+  } catch (e) {
+    res.status(403).send('Invalid Login');
+  }
+
+}
+
